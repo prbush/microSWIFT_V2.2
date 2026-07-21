d2e4c7921a85436b9ead1979673d62654cdc3e81
Laura Lindzey <lindzey@uw.edu>
2026-07-21 07:13:06 -0700

    Bump firmware version for release

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -61,7 +61,7 @@
 // clang-format on
 
 #define HARDWARE_VERSION (2U)
-#define FIRMWARE_VERSION (9U)
+#define FIRMWARE_VERSION (10U)
 
 static const microSWIFT_firmware_version_t firmware_version = {
     HARDWARE_VERSION, FIRMWARE_VERSION};
